b79175c67a5ea0f64b9fd90a29a0208d1c6098e2
vandhanab <112164783+vandhanab@users.noreply.github.com>
2024-09-18 21:55:25 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,15 @@
+var a=20;
+var b=30;
+var job="developer & trainer";
+var salary=999;
+var movie="salar";
+
+console.log(a);
+console.log(b);
+console.log(job);
+console.log(salary);
+console.log(movie);
+
+
+
+
